999c31cfb011e18dfe5f2a5361c39a125e4461ff
albim72 <marcin.albiniak72@gmail.com>
2023-09-05 14:14:17 +0200

    Update mojefunkcje.py

--- DZIEN_1/mojefunkcje.py
+++ DZIEN_1/mojefunkcje.py
@@ -16,3 +16,23 @@ print(g(5,5.6,2))
 print(g(5,1.1))
 print(g(y=1,x=8,z=3))
 print(n)
+
+# przykład 2
+
+def ranking(*lang,nrrank,**noweargs):
+    print(f'ranking nr {nrrank} -> pierwsze miejsce: {lang[0]}, drugie miejsce: {lang[1]},'
+          f'trzecie miejsce: {lang[2]}')
+
+def rankingk(k,nrrank):
+    print(f'ranking nr {nrrank} -> pierwsze miejsce: {k[0]}, drugie miejsce: {k[1]},'
+          f'trzecie miejsce: {k[2]}')
+
+
+ranking("Python","Java","C#","JavaScript",nrrank=34,a=45)
+
+lg = ("Python","Java","C#","JavaScript")
+rankingk(lg,4)
+
+rankingk("Python",45)
+
+
