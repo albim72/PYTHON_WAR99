80a823f5d8808918e5025ff1e5398c2aa59120b2
albim72 <marcin.albiniak72@gmail.com>
2023-09-05 15:22:19 +0200

    Update fanonimowa.py

--- DZIEN_1/fanonimowa.py
+++ DZIEN_1/fanonimowa.py
@@ -12,3 +12,22 @@ def multi(n):
     return lambda a:a+n
 
 print(multi(5.6)(4))
+
+
+liczba = [3,12,5,88,9,1,14,117,-4,0,24,-33,1,999,190,-45,101,1]
+
+parz = list(filter(lambda x:x%2==0,liczba))
+print(parz)
+
+cube = list(map(lambda x:x**3,liczba))
+print(cube)
+
+cube_s = set(map(lambda x:x**3,liczba))
+print(cube)
+
+nums = [k**4-3 for k in range(1000000)]
+print(nums)
+
+numsz = {k**4-3 for k in range(1000000)}
+print(numsz)
+
