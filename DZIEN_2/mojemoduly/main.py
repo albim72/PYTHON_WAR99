56f5e09a37ff533ccf83fada2828b4daabfcb06f
albim72 <marcin.albiniak72@gmail.com>
2023-09-06 13:16:55 +0200

    Update main.py

--- DZIEN_2/mojemoduly/main.py
+++ DZIEN_2/mojemoduly/main.py
@@ -3,6 +3,13 @@
 from dane import nrfilii as nf
 from dane import book as bk
 
+from funkcje.bfunkcje import czytaj_liste as cl, czytaj_slownik as cs
+
 print("_____ dane ______")
 print(nf)
 print(bk)
+
+print("_____ dane przez funkcje ______")
+cl(nf)
+print(".."*35)
+cs(bk)
