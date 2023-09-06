23a243f7d5efa5e1dfa2887650f8eb7d8b146d21
albim72 <marcin.albiniak72@gmail.com>
2023-09-06 14:26:15 +0200

    Create silniaerror.py

---
+++ DZIEN_2/silnia/silniaerror.py
@@ -0,0 +1,7 @@
+class SilniaError(Exception):
+    def __init__(self,n):
+        self.n=n
+        
+    def __str__(self):
+        return (f'[{self.__class__.__name__}] n nie może wynosić: {self.n}, silnia nie jest '
+                f'zdefiniowana dla liczb ujemnych')
