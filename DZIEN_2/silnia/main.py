3266a80998d6ec9a737fd3cd9bc4e66f8343f5c3
albim72 <marcin.albiniak72@gmail.com>
2023-09-06 14:48:15 +0200

    Update main.py

--- DZIEN_2/silnia/main.py
+++ DZIEN_2/silnia/main.py
@@ -6,6 +6,8 @@ import sys
 from silniaerror import SilniaError
 
 print(sys.version)
+sys.set_int_max_str_digits(0x1000000)
+sys.setrecursionlimit(0x2000000)
 
 def silnia(n):
     if n<0:
@@ -15,8 +17,18 @@ def silnia(n):
         wynik *= i
     return wynik
 
+def silnia_rek(n):
+    if n<0:
+        raise SilniaError(n)
+    if n==0:
+        return 1
+    else:
+        return n*silnia_rek(n-1)
+
+
 try:
     n = int(input("podaj wartość argumentu n funkcji silnia: "))
     print(f'wynik funckji silnia dla n = {n} wynosi {silnia(n)}')
+    print(f'wynik funckji rekurencyjnej silnia dla n = {n} wynosi {silnia_rek(n)}')
 except SilniaError as info:
     print(info)
