7665c75fea1e5121c838c4b641ea111ba87db4f1
albim72 <marcin.albiniak72@gmail.com>
2023-09-06 13:27:08 +0200

    Update main.py

--- DZIEN_2/mojemoduly/main.py
+++ DZIEN_2/mojemoduly/main.py
@@ -5,6 +5,8 @@ from dane import book as bk
 
 from funkcje.bfunkcje import czytaj_liste as cl, czytaj_slownik as cs
 
+from klasy.cdane import CDane
+
 print("_____ dane ______")
 print(nf)
 print(bk)
@@ -13,3 +15,9 @@ print("_____ dane przez funkcje ______")
 cl(nf)
 print(".."*35)
 cs(bk)
+
+print("_____ dane przez obiekt klasy ______")
+cd = CDane(nf,bk)
+cd.readl()
+print(".."*35)
+cd.readdict()
