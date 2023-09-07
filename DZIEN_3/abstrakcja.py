e3d3398f454496386b313cc007a8f1a1c2c124b4
albim72 <marcin.albiniak72@gmail.com>
2023-09-07 10:59:01 +0200

    Update abstrakcja.py

--- DZIEN_3/abstrakcja.py
+++ DZIEN_3/abstrakcja.py
@@ -5,14 +5,35 @@ class Prototyp(ABC):
         self.x = x
 
     def msg(self):
-        print("to jest wynik działania metody nieabstrakcyjnej!")
-        
+        return "to jest wynik działania metody nieabstrakcyjnej!"
+
     @abstractmethod
     def policz(self):
         pass
-    
+
     @abstractmethod
     def policz_x(self):
-        return self.x*7
-    
-    
+        e = self.x*7
+        return e*1.2
+
+
+class Regular(Prototyp):
+    def __init__(self,x,y):
+        super().__init__(x)
+        self.y = y
+
+    def policz(self):
+        return self.y - 0.06*self.x
+
+    def policz_x(self):
+        return super().policz_x() + 21*self.y
+
+    def msg(self):
+        return super().msg() + "... ciąg dalszy"
+
+
+rg = Regular(4,8)
+
+print(rg.msg())
+print(f'funkcja policz: {rg.policz()}')
+print(f'funkcja policz_x: {rg.policz_x()}')
