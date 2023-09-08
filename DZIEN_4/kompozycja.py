5328dec8fea132bcf110e954ae505e15239d7a75
albim72 <marcin.albiniak72@gmail.com>
2023-09-08 09:05:00 +0200

    Create kompozycja.py

---
+++ DZIEN_4/kompozycja.py
@@ -0,0 +1,22 @@
+class Address:
+    def __init__(self,street,city,country):
+        self.street = street
+        self.city = city
+        self.country = country
+
+class Person:
+    def __init__(self,name,address):
+        self.name = name
+        self.address = address
+
+class Employee:
+    def __init__(self,name,address,employee_id):
+        self.person = Person(name,address)
+        self.employee_id = employee_id
+
+address = Address("Złota 6/4 12-567","Kraków","Polska")
+employee = Employee("Jan Kot",address,45345)
+
+print(employee.person.name)
+print(employee.person.address.city)
+print(employee.employee_id)
