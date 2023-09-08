da37e32508491a2f648309aaf2de1a6a0b0d348f
albim72 <marcin.albiniak72@gmail.com>
2023-09-08 11:46:00 +0200

    Update main.py

--- DZIEN_4/dodajpracownika/main.py
+++ DZIEN_4/dodajpracownika/main.py
@@ -1,1 +1,20 @@
+import json
+
+def dodaj_nowego_pracownika(new_data,filename="pracownik.json"):
+    with open(filename,"r+",encoding="utf-8") as file:
+        file_data = json.load(file)
+        file_data["pra_info"].append(new_data)
+        print(len(file_data["pra_info"]))
+        file.seek(0)
+        json.dump(file_data,file,indent=4)
+
+nowyprac =  {
+            "imie": "Daniel",
+            "nazwisko": "Knotowski",
+            "stanowisko": "koordynator",
+            "lata_pracy": 1,
+            "email": "dani@firma.pl"
+        }
+
+dodaj_nowego_pracownika(nowyprac)
 
